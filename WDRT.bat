950087e3e6d273be0e60d644fc808d669e7de9e7
Repair My Computer <kevin.tyler.white@gmail.com>
2026-02-24 00:11:44 -0500

    Update WDRT.bat
    removed error message if user presses no to admin perm request.

--- WDRT.bat
+++ WDRT.bat
@@ -7,7 +7,7 @@ title WDRT v%APP_VERSION%
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo Requesting administrator privileges...
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    powershell -Command "Start-Process '%~f0' -Verb RunAs" 2>nul
     exit /b
 )
 
